7c173716c7ffcaded2999f6d872bf520273dce9c
princeps911 <princepsakpobasa911@gmail.com>
2024-09-26 08:14:54 -0700

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
-"""Contains top_ten function"""
+"""
+Contains the top_ten function
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit
+
+    Parameters:
+    subreddit (str): The subreddit to retrieve the top 10 hot posts for
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    try:
+        url = 'http://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+        headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        json_data = response.json()
+        posts = json_data.get("data", {}).get("children", [])
+        for post in posts[:10]:
+            print(post.get("data", {}).get("title", ""))
+
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 404:
+            print(None)
+        else:
+            print(f"Error: {e}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
