84065a8ea75ab82f59bd00188411b6e6580129be
princeps911 <princepsakpobasa911@gmail.com>
2024-09-26 07:43:28 -0700

    Update new 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,21 @@ Contains the number_of_subscribers function
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    try:
+        r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                         headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'})
+
+        r.raise_for_status()  # Raise an exception for bad status codes
+
+        json_data = r.json()
+        subs = json_data.get("data", {}).get("subscribers", 0)
+        return subs
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return 0
